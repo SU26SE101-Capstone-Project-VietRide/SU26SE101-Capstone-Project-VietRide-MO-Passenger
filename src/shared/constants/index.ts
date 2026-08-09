33a90ef23950b0a2a8127b890df7e734c0b4e6f4
Mac Viet Thong <thongmvse181966@fpt.edu.vn>
2026-08-10 02:00:20 +0700

    chore(assets): remove obsolete Figma localhost assets

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -4,7 +4,7 @@
 
 export { appConfig } from './config';
 export { isDemoMode, resolveDemoMode } from './demoMode';
-export { APP_LOGO, ASSETS } from './assets';
+export { APP_LOGO } from './assets';
 export { TOKEN_SERVICE_KEY } from './auth';
 
 
